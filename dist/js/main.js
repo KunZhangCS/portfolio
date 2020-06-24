d8a64658cc60b0bd2c13cd7b10dbd5345f69d4a4
Kun Zhang <kunzhang1988@gmail.com>
2020-06-24 12:24:17 +1000

    Change work flow, add links and change colors

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
+const sections = document.querySelectorAll('section')
 
 // Set Initial State of Menu
 let showMenu = false
@@ -19,13 +20,20 @@ function toggleMenu() {
         navItems.forEach(item => item.classList.add('show'))
 
         showMenu = true
-    } else {
-        menuBtn.classList.remove('close')
-        menu.classList.remove('show')
-        menuNav.classList.remove('show')
-        menuBranding.classList.remove('show')
-        navItems.forEach(item => item.classList.remove('show'))
-
-        showMenu = false
-    }
+    } else closeMenu()
 }
+
+// Close Menu when a navItem is clicked
+navItems.forEach(item => item.addEventListener('click', closeMenu))
+
+function closeMenu() {
+    menuBtn.classList.remove('close')
+    menu.classList.remove('show')
+    menuNav.classList.remove('show')
+    menuBranding.classList.remove('show')
+    navItems.forEach(item => item.classList.remove('show'))
+
+    showMenu = false
+}
+
+// Set active item in the menu depending on scroll position
